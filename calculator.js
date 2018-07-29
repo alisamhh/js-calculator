d0dae969e072e7ed12da58941b2643c36d9ee9b8
alisamhh <alisamhh@gmail.com>
2018-07-29 13:21:22 +1200

    refactoring calc

--- calculator.js
+++ calculator.js
@@ -4,37 +4,9 @@ var temp = "";
 $("button").click(function () {
     var val = $(this).text();
     if (!isNaN(val) || val === ".") {
-        if (temp == "" && val == "0" ||
-        temp.indexOf(".") > -1 && val === ".") {
-            return;
-        }
-        temp = temp + val;
-        $("#display").val(temp);
+        numberOrDecimal(val);
     } else if (val === "=") {
-        entries.push(temp);
-        var entriesLength = entries.length;
-        total = parseFloat(entries[0]);
-        for (var i = 1; i < entriesLength;) {
-            var nextEntry = parseFloat(entries[i + 1]);
-            switch (entries[i]) {
-                case "+":
-                    total += nextEntry;
-                    break;
-                case "-":
-                    total -= nextEntry;
-                    break;
-                case "*":
-                    total *= nextEntry;
-                    break;
-                case "/":
-                    total /= nextEntry;
-                    break;}
-
-            i = i + 2;
-        }
-        $("#display").val(total);
-        entries = [];
-        temp = total;
+        evaluateEquals();
     } else {
         var entriesLength = entries.length;
         if (entriesLength && isNaN(entries[entriesLength - 1])) {
@@ -53,3 +25,39 @@ $("#clear").click(function () {
     total = 0;
     temp = "";
 });
+
+const numberOrDecimal = function(val) {
+    if ((temp == "" && val == "0") ||
+        (temp.indexOf(".") > -1 && val === ".")) {
+        return;
+    }
+    temp = temp + val;
+    $("#display").val(temp);
+}
+
+const evaluateEquals = function() {
+    entries.push(temp);
+    var entriesLength = entries.length;
+    total = parseFloat(entries[0]);
+    for (var i = 1; i < entriesLength;) {
+        var nextEntry = parseFloat(entries[i + 1]);
+        switch (entries[i]) {
+            case "+":
+                total += nextEntry;
+                break;
+            case "-":
+                total -= nextEntry;
+                break;
+            case "*":
+                total *= nextEntry;
+                break;
+            case "/":
+                total /= nextEntry;
+                break;}
+
+        i = i + 2;
+    }
+    $("#display").val(total);
+    entries = [];
+    temp = total;
+}
